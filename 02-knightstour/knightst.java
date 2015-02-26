967951b9cb734ba0247ddaa82c55324cc60dee6a
Alice Xue <alicex123@gmail.com>
2015-02-25 21:54:43 -0500

    completed

--- 02-knightstour/knightst.java
+++ 02-knightstour/knightst.java
@@ -6,19 +6,14 @@ public class knightst {
     private int maxX = 5;
     private int maxY = 5;
     private boolean solved = false;
-    private int step = 1;
-
-    public void delay(int n){
-	try {
-	    Thread.sleep(n);
-	} catch (Exception e) {}
-    }
 
     public knightst() {
-	board = new int[maxX+4][maxY+4];
-	for (int i=0; i<maxX+2; i++) {
-	    for (int j=0; j<maxY+2; j++) {
-		if (i<2 || i>=maxX || j<2 || j>=maxX) {
+	maxX+=4;
+	maxY+=4;
+	board = new int[maxX][maxY];
+	for (int i=0; i<maxX; i++) {
+	    for (int j=0; j<maxY; j++) {
+		if (i<2 || i>=maxX-2 || j<2 || j>=maxX-2) {
 		    board[i][j]=-1;
 		} else {
 		    board[i][j]=0;
@@ -28,13 +23,10 @@ public class knightst {
     }
     
     public void print() {
-	for (int i=0;i<=board.length;i++) {
+	for (int i=0;i<board.length;i++) {
 	    for (int j=0;j<board[0].length;j++) {
 		if (board[i][j]!=-1) {
 		    System.out.printf("|%2d",board[i][j]);
-		}
-		if (i==board.length-2) {
-		    System.out.printf("|");
 		}
 	    }
 	    System.out.printf("\n");
@@ -42,32 +34,31 @@ public class knightst {
     }
 		
 
-    public void solve(int x, int y) {
-	if (board[x][y]!=0 || board[x][y]==-1 || solved) {
+    public void solve(int x, int y, int step) {
+	if (board[x][y]!=0 || solved) {
 	    return;
 	}
-	if (step==maxX*maxY) {
+	board[x][y]=step;
+	if (step==(maxX-4)*(maxY-4)) {
+	    print();
 	    solved = true;
 	}
-	delay(100);
-	solve(x+2,y+1);
-	solve(x+2,y-1);
-	solve(x+1,y+2);		
-	solve(x+1,y-2);      
-	solve(x-2,y+1);
-	solve(x-2,y-1);
-	solve(x-1,y+2);
-	solve(x-1,y-2);
+	solve(x+2,y+1,step+1);
+	solve(x+2,y-1,step+1);
+	solve(x+1,y+2,step+1);		
+	solve(x+1,y-2,step+1);      
+	solve(x-2,y+1,step+1);
+	solve(x-2,y-1,step+1);
+	solve(x-1,y+2,step+1);
+	solve(x-1,y-2,step+1);
 	if (!solved) {
-	    board[x][y]=step;
-	    step++;
+	    board[x][y]=0;
+	    step--;
 	}
     }
 
     public static void main(String[] args) {
 	knightst k = new knightst();
-	System.out.println(k);
-	k.solve(2,2);
-	System.out.println(k);
+	k.solve(2,2,1);
     }
 }
