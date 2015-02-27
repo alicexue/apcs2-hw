24150815473699b232610610151518a70c430980
Alice Xue <alicex123@gmail.com>
2015-02-27 08:40:11 -0500

    x

--- 04-mergesort/mergeSort.java
+++ 04-mergesort/mergeSort.java
@@ -9,6 +9,10 @@ public class mergeSort {
     public ArrayList<Integer> mergeSort (ArrayList<Integer> a) {
 	a1 = new ArrayList<Integer>();
 	a2 = new ArrayList<Integer>();
+	if (a.size()==0) {
+	    System.out.println("a");
+	    return a;
+	}
 	for (int i=0;i<a.size();i++) {
 	    if (i<a.size()/2) {
 		a1.add(a.get(i));
@@ -26,7 +30,7 @@ public class mergeSort {
     public ArrayList<Integer> merge(ArrayList<Integer> a1, ArrayList<Integer> a2) {
 	ArrayList<Integer> m = new ArrayList<Integer>();
 	while(a1.size() > 0 || a2.size() > 0) {
-	    if(a1.size() == 0) {
+	    if (a1.size() == 0) {
 	        while (a2.size()>0) {
 		    m.add(a2.remove(0));
 		}
@@ -36,26 +40,24 @@ public class mergeSort {
 		}
 	    } else if(a1.get(0) < a2.get(0)) {
 		m.add(a1.remove(0));
-		System.out.println(a1 + "1");
-		System.out.println(m);
+		m.add(a2.remove(0));
 	    } else {
 		m.add(a2.remove(0));
-		System.out.println(a2 + "2");
-		System.out.println("m: "+m);
+		m.add(a1.remove(0));
 	    }
 	}
 	return m;
     }
+
 
     public static void main (String[] args) {
 	mergeSort m = new mergeSort();
 	ArrayList<Integer> q = new ArrayList<Integer>();
 	q.add(1);
 	q.add(2);
-	q.add(3);
+	q.add(5);
+	q.add(2);
 	q.add(4);
-	q.add(4);
-	System.out.println(m.merge(q,q));
-	//System.out.println(m.mergeSort(q));
+	System.out.println(m.mergeSort(q));
     }
 }
