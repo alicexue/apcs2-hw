1d6b73acae2dde04d2791f251f87a75e8962236d
Alice Xue <alicex123@gmail.com>
2015-03-20 08:41:32 -0400

    x

--- 09-iterator/LLit.java
+++ 09-iterator/LLit.java
@@ -2,22 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
-		private Node<E> t;
-		public LLit(Node<E> n){
-				t=n;
-		}
+    private Node<E> t;
+    private E last;
+    
+    public LLit(Node<E> n){
+	t=n;
+    }
 
-		public boolean hasNext(){
-				return t!=null;
-		}
+    public boolean hasNext(){
+	return t!=null;
+    }
 
-		public E next(){
-				E retval = t.getData();
-				t=t.getNext();
-				return retval;
-		}
+    public E next(){
+	E retval = t.getData();
+	t=t.getNext();
+	return retval;
+    }
 
-		public void remove() {
-				// fill this in later
-		}
+    public void remove() {
+        Node tmp;
+	Node prev = t;
+	for (tmp = t.getNext(); tmp!=null; tmp=tmp.getNext()) {
+	    t = t.getNext();
+	    if (tmp.getData() = last) {
+		
+	    }
+	}
+    }
+		
+    }
 }
