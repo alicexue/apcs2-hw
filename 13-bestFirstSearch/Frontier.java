f618173147f3fa012dad57f0f40fe095bd48d71f
Alice Xue <alicex123@gmail.com>
2015-04-19 21:47:40 -0400

    has issues

--- 13-bestFirstSearch/Frontier.java
+++ 13-bestFirstSearch/Frontier.java
@@ -18,14 +18,17 @@ public class Frontier {
     public void addPrio(Node n) {
 	if (l.isEmpty()) {
 	    l.add(n);
+	} else if (l.get(l.size()-1).getPrio()<n.getPrio()) {
+		l.add(n);
 	} else {
 	    int i = 0;
-	    while (i < l.size()) {
-		if (n.getPrio()<l.get(i).getPrio()) {
-		    l.get(i).setPrev(n);
-		} else {
-		    l.add(n);
-		}
+	    double prio = n.getPrio();
+	    while (i<l.size()) {
+	    	if (prio<l.get(i).getPrio()) {
+	    		l.add(i,n);
+	    		return;
+	    	} 
+	    	i++;
 	    }
 	}
     }
--- 13-bestFirstSearch/Maze.java
+++ 13-bestFirstSearch/Maze.java
@@ -17,19 +17,18 @@ public class Maze
     private Frontier f;
 		
     public void delay(int n){
-	try {
+    	try {
 	    Thread.sleep(n);
-	} catch (Exception e) {}
+    	} catch (Exception e) {}
     }
 		
-    public Maze() 
-    {
+    public Maze() {
 	maxX=40;
 	maxY=20;
 	board = new char[maxX][maxY];
-				
+					
 	try {
-						
+							
 	    Scanner sc = new Scanner(new File("maze.dat"));
 	    int j=0;
 	    while (sc.hasNext())
@@ -42,148 +41,113 @@ public class Maze
 		    j++;
 		}
 	}
-	catch (Exception e)
-	    {
-	    }
-				
+	catch (Exception e){
+	}
+					
     }
 		
-    public String toString()
-    {
+    public String toString(){
 	String s = "[2J\n";
-				
-	for (int y=0;y<maxY;y++)
-	    {
-		for (int x=0;x<maxX;x++)
-		    s = s +board[x][y];
-		s=s+"\n";
-	    }
+					
+	for (int y=0;y<maxY;y++){
+	    for (int x=0;x<maxX;x++)
+		s = s +board[x][y];
+	    s=s+"\n";
+	}
 	return s;
     }
 
-    /*
-      solved - instance variable to indicate we're done
-			
-    */
-    public void solve(int x, int y){
-	if (board[x][y]==wall ||
-	    board[x][y]==me ||
-	    board[x][y]==visited ||
-	    solved){
-	    return;
-	}
-
-	if (board[x][y]==exit){
-	    System.out.println(this);
-	    solved = true;
-	}
-	delay(100);
-	System.out.println(this);
-	board[x][y]=me;
-	solve(x+1,y);
-	solve(x-1,y);
-	solve(x,y+1);
-	solve(x,y-1);
-	if (!solved){
-	    board[x][y]=visited;
-	}
-    }
-
-    /*
-      Only adds if the tx,ty spot is available path or exit
-    */
-    public void addToFront(int tx,int ty, Node current){
-	Node tmp = null;
-	if (board[tx][ty]=='#' || board[tx][ty]=='$'){
-	    tmp = new Node(tx,ty);
-	    tmp.setPrev(current);
-	    f.add(tmp);
-	}
-						
-    }
-
-    public int[] findexit() {
-	int[] result = new int[2];
-	for (int y=0;y<maxY;y++) {
-	    for (int x=0;x<maxX;x++)
-		if (board[x][y]=='$') {
-		    result[0]=x;
-		    result[1]=y;
-		    			     
-	}
-    }
     
-    public void bfs(int x, int y){
-	//f = new Frontier();
-	f = new StackFront();
-
-	f.add(new Node(x,y));
-
-	int tx=0,ty=0;
-	Node current = null;
-	while (!f.isEmpty()){
-	    current = f.remove();
-	    int cx = current.getX();
-	    int cy = current.getY();
-
-	    if (board[cx][cy]=='$')
-		break;
-						
-	    board[cx][cy]='z';
-
-	    addToFront(cx+1,cy,current);
-	    addToFront(cx-1,cy,current);
-	    addToFront(cx,cy+1,current);
-	    addToFront(cx,cy-1,current);
-
-	    delay(50);
-	    System.out.println(this);
-	}
-
-	// path recovery
-	for (Node p = current.getPrev(); p != null ; p = p.getPrev()){
-	    board[p.getX()][p.getY()] = 'P';
-	    delay(100);
-	    System.out.println(this);
-	}
+    public boolean pathexit(int x, int y) {
+    	if (board[x][y]=='#' || board[x][y]=='$') {
+	    return true;
+    	} else {
+	    return false;
+    	}
+    	
     }
 
     public void bestfs(int x, int y){
 	//f = new Frontier();
 	f = new StackFront();
-
+	
 	Node n = new Node(x,y);
-	n.setPrio(////
+	//n.setPrio(euclid(n));
+	n.setPrio(manhattan(n));
 	f.addPrio(new Node(x,y));
-
+	
 	int tx=0,ty=0;
 	Node current = null;
 	while (!f.isEmpty()){
 	    current = f.remove();
 	    int cx = current.getX();
 	    int cy = current.getY();
-
-	    if (board[cx][cy]=='$')
+	
+	    if (board[cx][cy]=='$') {
+		solved = true;
 		break;
-						
+	    }				
+			
 	    board[cx][cy]='z';
-
-	    addToFront(cx+1,cy,current);
-	    addToFront(cx-1,cy,current);
-	    addToFront(cx,cy+1,current);
-	    addToFront(cx,cy-1,current);
-
+		
+	    if (pathexit(cx+1,cy)) {
+		f.addPrio(new Node(cx+1,cy));
+	    }
+	    if (pathexit(cx,cy+1)) {
+		f.addPrio(new Node(cx,cy+1));
+	    }
+	    if (pathexit(cx-1,cy)) {
+		f.addPrio(new Node(cx-1,cy));
+	    }
+	    if (pathexit(cx,cy-1)) {
+		f.addPrio(new Node(cx,cy-1));
+	    }
 	    delay(50);
 	    System.out.println(this);
 	}
+	/*
+	//	if (!solved) {	
+	for (Node b = current.getPrev(); b!=null; b=b.getPrev()) {
+	    board[b.getX()][b.getY()] = '#';
+	    delay(50);
+	    System.out.println(this);
+	}
+	
+	//	}
+	*/
+    }
+	
+    public Node findexit(){
+	Node result = new Node(0,0);
+	int j=0;
+	for (int i=0;i<maxX;i++){
+	    if (board[i][j] == '$') 
+		result = new Node(i,j);
+	}
+	j++;
+	return result;
+    }
 
+    public double euclid(Node n) {
+	Node exit = findexit();
+	double x = (exit.getX()-n.getX())*(exit.getX()-n.getX());
+	double y = (exit.getY()-n.getY())*(exit.getY()-n.getY());
+	return Math.sqrt(x+y);
+    }
+	
+    public int manhattan(Node n){
+	Node exit = findexit();
+	int x = Math.abs(exit.getX() - n.getX());
+	int y = Math.abs(exit.getY() - n.getY());
+	return x + y;
+    }
+	
+    //public int 
 	
     public static void main(String[] args){
 	Maze m = new Maze();
 	System.out.println(m);
-	m.bfs(1,1);
+	m.bestfs(1,1);
 	System.out.println(m);
-		
     }
 }
-
