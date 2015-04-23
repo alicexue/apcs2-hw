5f2558499dd5a3b420377fd696071017097be476
Alice Xue <alicex123@gmail.com>
2015-04-23 17:24:48 -0400

    added stuff but didn't test it yet

--- 14-tree/Tree.java
+++ 14-tree/Tree.java
@@ -1,13 +1,39 @@
-public class Tree {
-    
-    Tree T = new Tree();
-    
+public class Tree {    
+    private Node T;
+
     public void insert(int a) {
 	if (T==null)
 	    T = new Node(a);
+	else {
+	    Node tmp = T;
+	    while (T!=null) {
+		int c = T.getData().compareTo(i);
+		tmp = T;
+		if (c>0) {
+		    T = T.getRight();
+		} else if (c<0) {
+		    T = T.getLeft();
+		} else {
+		    break;
+		}
+	    }
+	    if (a<T.getData) 
+		tmp.setLeft(new Node(a));
+	    else 
+		tmp.setRight(new Node(a));
+	}
     }
 
     public Node search(Node T, int i) {
-
+	while (T!=null) {
+	    int c = T.getData().compareTo(i);
+	    if (c>0) 
+		T = T.getRight();
+	    else if (c<0)
+		T = T.getLeft();
+	    else 
+		return T;
+	}
+	return null;
     }
 }
---
+++ 14-tree/Tree.java~
@@ -0,0 +1,13 @@
+public class Tree {
+    
+    Tree T = new Tree();
+    
+    public void insert(int a) {
+	if (T==null)
+	    T = new Node(a);
+    }
+
+    public Node search(Node T, int i) {
+
+    }
+}
