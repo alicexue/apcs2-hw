d7e82855510398c96e12d3ff6261a08983360a51
Alice Xue <alicex123@gmail.com>
2015-04-27 08:38:41 -0400

    x

--- 14-tree/BST.java
+++ 14-tree/BST.java
@@ -4,6 +4,25 @@ public class BST {
     public Tree() {
 	T = null;
     }
+
+    public Node search (Node n, int i) {
+	if (n==null || n.getData()==i) {
+	    return n;
+	} else if (i<n.getData()) {
+	    return search(n.getLeft(),i);
+	} else (i>n.getData()) {
+	    return search(n.getRight(),i);
+	}
+    }
+
+    public String search(int i) {
+	Node n = search(T,i);
+	if (n==null) {
+	    return "NOT FOUND";
+	} else {
+	    return n.toString();
+	}
+    }
     
     public void insert(int a) {
 	if (T==null)
@@ -11,26 +30,27 @@ public class BST {
 	else {
 	    Node tmp = T;
 	    while (T!=null) {
-		int c = compareTo(a);
 		tmp = T;
-		if (c>0) 
+		if (T.getData()>a) 
 		    T = T.getRight();
-		else if (c<0) 
+		else if (T.getData()<0) 
 		    T = T.getLeft();
-		if (a<tmp.getData()) 
-		    tmp.setLeft(new Node(a));
-		else 
-		    tmp.setRight(new Node(a));
+		else
+		    return;	       
 	    }
+	    if (a<tmp.getData()) 
+		tmp.setLeft(new Node(a));
+	    else 
+		tmp.setRight(new Node(a));
 	}
     }
 
     public Node search(int i) {
 	while (T!=null) {
 	    int c = compareTo(i);
-	    if (c>0) 
+	    if (T.getData()>i) 
 		T = T.getRight();
-	    else if (c<0)
+	    else if (T.getData()<i)
 		T = T.getLeft();
 	    else 
 		return T;
@@ -38,13 +58,13 @@ public class BST {
 	return null;
     }
 
-    public int compareTo(int a) {
-	if (T.getData()<a) 
-	    return -1;
-	else if (T.getData()>a) 
-	    return 1;
-	else
-	    return 0;
+    public String traverse(Node T) {
+	String s = "";
+	if (T==null) {
+	    return s;
+	} else {
+	    s = s + T.getData + " (" traverse(T.getLeft()) + ", " + traverse(T.getRight) + ")");
+	}
     }
 
 }
